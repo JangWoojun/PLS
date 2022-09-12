dc477d6dddc0d5bc0ea2cc7e2760c0d787f2ee70
JangWoojun <woojun0107@naver.com>
2022-09-12 16:39:48 +0900

    파일 이동

---
+++ C/self/4.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+int main() {
+    int a, b;
+    a = 10;
+    b = 5;
+
+    printf("a + b 는: %d \n", a + b); 
+    printf("a - b 는: %d \n", a - b);
+    printf("a * b 는: %d \n", a * b); 
+    printf("a / b 는: %d \n", a / b); 
+    printf("a %% b 는: %d \n", a % b); 
+    
+}
