0665376ad211e4f2770baaaacecb699dc3bdaa66
JangWoojun <woojun0107@naver.com>
2022-09-15 16:09:21 +0900

    프로그래밍 파이썬 수업 / 주석 추가

--- Python/10.py
+++ Python/10.py
@@ -5,7 +5,7 @@ def wc2(st):
     w = len(st.split(" "))
     c = len(st)-st.count(" ")
     b = st.count(" ")
-    return w,c,b
+    return w,c,b ## 공백문자를 기준으로 나누고 그 공백에 길이와 자신-공백한 값, 공백 문자의 수를 리턴해준다
 
 
 print(wc2("힘들어 용"))
--- Python/9.py
+++ Python/9.py
@@ -3,7 +3,7 @@ from calendar import c
 
 def wc(st):
     c = len(st)-st.count(" ")
-    return c
+    return c ### 원래 값에 길이에 원래 값에 있는 공백문자를 빼서 리턴
 
 
 print(wc("힘들어 용"))
