67f6ceffad4098cd5ddf498801b2a97fbf49ab87
JangWoojun <woojun0107@naver.com>
2022-09-15 16:09:18 +0900

    프로그래밍 파이썬 수업 / 주석 추가

--- Python/7.py
+++ Python/7.py
@@ -1,9 +1,9 @@
 def primenum (s,e):
-    a=[]
-    for i in range(s,e+1):
-        chk = 1
-        for j in  range (2,i):
-            if i % j == 0:
+    a=[] 
+    for i in range(s,e+1): # 받은 s부터 e값까지 for문 돌리고
+        chk = 1 #chk로 소수가 아니면 0으로 체크
+        for j in  range (2,i): # e부터 자신 -1 값까지 for문으로 약수인지 확인
+            if i % j == 0: ## 없으면 a에 담겨서 리턴
                 chk=0
                 break
         if chk == 1:
--- Python/8.py
+++ Python/8.py
@@ -1,7 +1,7 @@
 def login (id1,pw2):
     id="a"
     pw="b"
-    if id==id1 and pw == pw2:
+    if id==id1 and pw == pw2: ## 값을 입력 받아서 이미 있는 값과 같으면 true 리턴 틀리면 false 리턴
         return True
     else:
         return False
