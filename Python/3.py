7d7a04cb03f8b2e2967fb9d8f6e502ffe9baaf86
JangWoojun <woojun0107@naver.com>
2022-09-15 15:38:02 +0900

    프로그램 수업

---
+++ Python/3.py
@@ -0,0 +1,6 @@
+def circle_area(a): #지름을 a로 받는다
+    return(a/2)*(a/2)*3.14 # 반으로 나눠서 반지름으로 만들고 (반지름)×(반지름)×(원주율)해서 리턴
+    
+
+
+print (circle_area(5))
---
+++ Python/4.py
@@ -0,0 +1,6 @@
+def tri_area(a,b): #밑변과 높이를 a,b로 받는다
+    return a/2*b # ½ × (밑변) × (높이)를 해주고 리턴
+    
+
+
+print (tri_area(5,10))
