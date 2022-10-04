faad935ac8a436e14e099a46cb9c3c1db922da5d
JangWoojun <woojun0107@naver.com>
2022-10-04 21:39:08 +0900

    2022/10/04 과제

---
+++ Kotlin/5.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var humidity = "humid"
+    var humidityLevel = 80
+
+    while (humidity == "humid"){
+
+        humidityLevel-=5
+        println("humidity decreased")
+
+        if(humidityLevel < 60){
+            humidity = "comfy"
+            println("it's comfy now")
+        }
+    }
+}
