1cb002ed5f06fdc36b5b10202819065b9b90ae0f
JangWoojun <woojun0107@naver.com>
2022-10-04 21:10:43 +0900

    2022/10/04

---
+++ Kotlin/3.kt
@@ -0,0 +1,19 @@
+fun main(){
+    // for(num in 1..10) {
+    //     print("$num ")
+    // }
+    // print("\n")
+    // for(i in 1 until 10) { 
+    //     print("$i ")
+    // }
+ 
+    for(i in 10 downTo 1) {     
+        print("$i ")
+    }
+ 
+    print("\n")
+
+    for(i in 1 until 10 step 2) { 
+        print("$i ")
+    }
+}
