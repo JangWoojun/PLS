98cccd41e175e3906e2bb82f8cfc8519d36dc2fd
JangWoojun <woojun0107@naver.com>
2022-10-05 21:34:27 +0900

    2022/10/05

---
+++ Kotlin/6.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var z = 12
+    do {
+        print("$z ")
+    } while(z <= 10)
+}
---
+++ Kotlin/9.kt
@@ -0,0 +1,8 @@
+fun main(){
+    for (i in 1..20){
+        if(i/2==5){
+            continue
+        }
+        println(i)
+    }
+}
