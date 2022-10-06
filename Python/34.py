e5b24c24f9e7d52f08a988c1f65847cedb7d88f0
JangWoojun <woojun0107@naver.com>
2022-10-06 16:00:59 +0900

    2022/10/06

--- Python/34.py
+++ Python/34.py
@@ -2,12 +2,12 @@ import os
 
 def read_folder(path):
     print(path)
-    output=os.listdir(path)
+    output=os.listdir(path) # 현재 경로에 정보를 output에 담고
     
-    for item in output:
-        if os.path.isdir(path + "/"+item):
-            read_folder(path + "/"+item)
+    for item in output: # output에 있는 정보들을 하나씩 아이템에 담아서
+        if os.path.isdir(path + "/"+item): #만약 이것이 폴더라면
+            read_folder(path + "/"+item) # 재귀로 해당 함수를 한번더 호출 
         else:
-            print("파일 :",item)
+            print("파일 :",item) #아니라면 출력
             
 read_folder(".")
