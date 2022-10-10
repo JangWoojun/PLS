93b39e60551cdd5ffacc3cc519c6cbfd8bf9a603
JangWoojun <woojun0107@naver.com>
2022-10-11 04:51:11 +0900

    2022/10/11

---
+++ Kotlin/25.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val number1:IntArray = intArrayOf(1,2,3,4,5)
+    val number2 = intArrayOf(1,2,3,4,5)
+    val number3 = arrayOf(1,2,3,4,5)
+
+    println(number1)
+    println(number1.contentToString())
+    
+}
