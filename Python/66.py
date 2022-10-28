680fb69510e9d6bf2b0054af70404ef54f8ab43c
JangWoojun <woojun0107@naver.com>
2022-10-29 00:53:49 +0900

    2022/10/29

---
+++ Python/66.py
@@ -0,0 +1,9 @@
+print("{0: >10}".format(500))
+
+print("{0: >+10}".format(500))
+print("{0: >-10}".format(-500))
+
+print("{0:_<+10}".format(500))
+print("{0:_<-10}".format(-500))
+
+print("{0:+,}".format(10000000000))
