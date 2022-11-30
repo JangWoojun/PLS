0816f81de9961d3942915060d1aa2345aa4216a7
JangWoojun <woojun0107@naver.com>
2022-11-30 17:20:39 +0900

    2022/11/30

---
+++ Swift/3.swift
@@ -0,0 +1,13 @@
+var name = "Swift"
+var age = 9
+
+print(name,age)
+
+print("\(name)(은)는 \(age) 전에 만들어졌습니다")
+
+name = "파이썬"
+age = 12
+
+print(name,age)
+
+print("\(name)(은)는 \(age) 전에 만들어졌습니다")
---
+++ Swift/4.swift
@@ -0,0 +1,6 @@
+var a = 3
+var b = 32
+
+let c = a+b
+
+print
