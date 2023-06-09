f99e2b22f63b8ec273166b1dead02226c45fb327
JangWoojun <woojun0107@naver.com>
2023-06-09 21:19:42 +0900

    2023/06/09

---
+++ Dart/3.dart
@@ -0,0 +1,21 @@
+void main() {
+  // 정수형 변수 선언과 초기화
+  int age = 30;
+  print("나이: $age");
+
+  // 문자열 변수 선언과 초기화
+  String name = "John Doe";
+  print("이름: $name");
+
+  // 부동 소수점 변수 선언과 초기화
+  double salary = 2500.50;
+  print("월급: $salary");
+
+  // 논리형 변수 선언과 초기화
+  bool isEmployed = true;
+  print("취업 여부: $isEmployed");
+
+  // 변수 값 변경
+  age = 35;
+  print("새로운 나이: $age");
+}
