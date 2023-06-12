a13a1cc05f78d96f4c7b729385f7e20cea09b2eb
JangWoojun <woojun0107@naver.com>
2023-06-12 14:18:27 +0900

    2023/06/12

---
+++ Dart/6.dart
@@ -0,0 +1,21 @@
+void main() {
+  String fruit = 'apple';
+
+  switch (fruit) {
+    case 'apple':
+      print('Selected fruit is apple.');
+      break;
+
+    case 'banana':
+      print('Selected fruit is banana.');
+      break;
+
+    case 'orange':
+      print('Selected fruit is orange.');
+      break;
+
+    default:
+      print('Selected fruit is not apple, banana, or orange.');
+      break;
+  }
+}
