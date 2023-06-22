48864a15025ab0707b3b6ad4f1a5e22fdde36683
JangWoojun <woojun0107@naver.com>
2023-06-22 19:42:05 +0900

    2023/06/22

---
+++ Dart/15.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 25;
+  bool isMan = true;
+  String name = "John";
+  double height = 175.5;
+  
+  print(age.runtimeType);   // int
+  print(isMan.runtimeType); // bool
+  print(name.runtimeType);  // String
+  print(height.runtimeType);  // double
+}
