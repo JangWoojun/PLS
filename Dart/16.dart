4546ba30dc82dd84093160663a7f41fcc929cdcb
JangWoojun <woojun0107@naver.com>
2023-06-23 12:06:40 +0900

    2023/06/23

---
+++ Dart/16.dart
@@ -0,0 +1,18 @@
+void main() {
+    int a = 10;
+    int b = 3;
+
+    int sum = a + b; // 덧셈
+    int difference = a - b; // 뺄셈
+    int product = a * b; // 곱셈
+    double quotient = a / b; // 나눗셈
+    int remainder = a % b; // 나머지
+    int negative = -a; // 부정
+
+    print(sum); // 13
+    print(difference); // 7
+    print(product); // 30
+    print(quotient); // 3.3333333333333335
+    print(remainder); // 1
+    print(negative); // -10
+}
