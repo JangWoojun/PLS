146ecd620e07502705b5169283afbe49c1a6f81f
JangWoojun <woojun0107@naver.com>
2023-09-24 21:34:51 +0900

    2023/09/24

---
+++ Kotlin/35.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val a = 1
+    val b = 2
+
+    // if식
+    val max = if (a > b) a else b
+    print(max)
+
+    // if문
+    if (a > b) {
+        println("a가 더 큼")
+    } else {
+        println("b가 더 큼")
+    }
+    
+}
