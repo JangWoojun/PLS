90320eb6b2bdac08c004f1fa94c2e71799efca9c
JangWoojun <woojun0107@naver.com>
2023-09-26 23:20:38 +0900

    2023/09/26

---
+++ Dart/23.dart
@@ -0,0 +1,13 @@
+void main() {
+    int result1 = 7 ~/ 3;  // 결과는 2
+    int result2 = 10 ~/ 4; // 결과는 2
+
+    print(result1); // 2를 출력
+    print(result2); // 2를 출력
+
+    // int result3 = 7 / 3;  // 오류 double 형
+    // int result4 = 10 / 4; // 오류 double 형
+
+    // print(result4); // 오류
+    // print(result3); // 오류
+}
