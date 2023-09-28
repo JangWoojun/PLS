9eec83f67612defd66ce6bb3dc79eac92035713c
JangWoojun <woojun0107@naver.com>
2023-09-28 14:42:10 +0900

    2023/09/28

---
+++ Dart/25.dart
@@ -0,0 +1,33 @@
+void main() {
+    // bool 형
+    bool isTrue = true;
+
+    // int 형
+    int number1 = 1;
+
+    // double 형
+    double number2 = 1.0;
+
+    // string 형
+    String text = "Hello World";
+
+    // null 형
+    Null thisNull = null;
+
+    print(isTrue);
+    print(number1);
+    print(number2);
+    print(text);
+    print(thisNull);
+
+    // 가변형 var
+    var value1 = 1;
+    value1 = 2;
+
+    // 가변형 dynamic
+    dynamic value2 = 2;
+    value2 = "3";
+
+    print(value1);
+    print(value2);
+}
