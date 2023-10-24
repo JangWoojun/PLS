c1d4c6e12238adb772544662e1b7d9c1661c043c
JangWoojun <woojun0107@naver.com>
2023-10-24 21:06:04 +0900

    2023/10/24

---
+++ Python/98.py
@@ -0,0 +1,19 @@
+def shell_sort(arr):
+    n = len(arr)
+    gap = n // 2
+    
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+
+    return arr
+
+# 예시
+arr = [35, 33, 42, 10, 14, 19, 27, 44]
+print(shell_sort(arr))
