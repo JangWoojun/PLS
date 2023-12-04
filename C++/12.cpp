104c9a7ae7b9401249d7f626b9cd0134669f5657
JangWoojun <woojun0107@naver.com>
2023-12-05 08:56:45 +0900

    2023/12/05

---
+++ C++/12.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define five 5
+#define isFive(a) if (a == five)
+int a;
+
+int main() {
+    cout << five << "를 입력하세요" << "\n";
+    cin >> a;
+
+    isFive(a) {
+        cout << "5 입니다" << "\n";
+    } else {
+        cout << "5가 아닙니다" << "\n";
+    }
+
+    return 0;
+}
