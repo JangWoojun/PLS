d5ffc385decf7848e9e9294ef07f80d3da74d84b
JangWoojun <woojun0107@naver.com>
2023-12-06 23:03:19 +0900

    2023/12/06

---
+++ C++/14.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int a;
+    double b;
+
+    std::cout << "Size of int: " << sizeof(a) << " bytes" << std::endl;
+    std::cout << "Size of double: " << sizeof(b) << " bytes" << std::endl;
+
+    return 0;
+}
