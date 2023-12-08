398cea32914adfcfb2cafa155a73e485ebcbba0f
JangWoojun <woojun0107@naver.com>
2023-12-08 12:06:30 +0900

    2023/12/08

---
+++ C++/20.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char myChar = 'A';
+    printf("%c\n", myChar); // 'A'를 출력
+
+    char myString[] = "Hello";
+    printf("%s\n", myString); // "Hello"라는 문자열을 출력
+}
