6c80f57a475c71fb5d78ce8f36c388ab0608304d
SUS <woojun0107@naver.com>
2023-12-15 21:27:25 +0900

    2023/12/15

---
+++ C++/36.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+enum class State { Idle, Running, Paused, Stopped };
+enum class MyEnum : char { A, B, C };
+
+void updateState(State currentState) {
+    // 상태에 따른 로직 수행
+    // ...
+}
+
+fun main() {
+  State currentState = State::Running;
+
+  return 0;
+}
