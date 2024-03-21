8a41961d22a6c88507809890207013e9c2fd3101
JangWoojun <woojun0107@naver.com>
2024-03-21 22:37:37 +0900

    2024/03/21

---
+++ JS/10.js
@@ -0,0 +1,14 @@
+let expression = 10;
+
+switch (expression) {
+    case 1:
+        console.log("1");
+        break;
+    case 10:
+        console.log("10");
+        break;
+    default:
+        console.log("없음");
+        break;
+}
+  
