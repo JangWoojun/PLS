4f53797fffc8b8827000e85e3f8e63b16aa61218
JangWoojun <woojun0107@naver.com>
2024-03-23 17:40:11 +0900

    2024/03/23

---
+++ JS/18.js
@@ -0,0 +1,9 @@
+let c = 5, d = 10;
+console.log(c < d); // 작다: true
+console.log(c > d); // 크다: false
+console.log(c <= 5); // 작거나 같다: true
+console.log(c >= 5); // 크거나 같다: true
+console.log(c == "5"); // 동등: true (타입은 비교하지 않음)
+console.log(c === "5"); // 일치: false (타입도 비교)
+console.log(c != "10"); // 부등: true
+console.log(c !== "5"); // 불일치: true
