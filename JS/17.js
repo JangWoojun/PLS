26dcfec41d4da1712ef452e03f666177b09066d0
JangWoojun <woojun0107@naver.com>
2024-03-23 17:39:41 +0900

    2024/03/23

---
+++ JS/17.js
@@ -0,0 +1,7 @@
+let a = 10, b = 3;
+console.log(a + b); // 덧셈: 13
+console.log(a - b); // 뺄셈: 7
+console.log(a * b); // 곱셈: 30
+console.log(a / b); // 나눗셈: 3.333...
+console.log(a % b); // 나머지: 1
+console.log(a ** b); // 거듭제곱: 1000
